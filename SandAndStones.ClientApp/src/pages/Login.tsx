744a02ed16ccf9a2bbf5acc3fe435442770c8991
sandandstonesdev <136804658+sandandstonesdev@users.noreply.github.com>
2024-11-19 11:58:30 +0100

    Zod Validation for pass/user in Register/Login Screens (#59)

--- SandAndStones.ClientApp/src/pages/Login.tsx
+++ SandAndStones.ClientApp/src/pages/Login.tsx
@@ -1,17 +1,25 @@
 import { useEffect, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { axiosInstance } from "../hooks/useAxios";
+import { formatZodIssue } from "../hooks/useZodFormatIssue";
 import axios, { isAxiosError } from "axios";
 import { AuthData } from "../types/AuthData";
 import { useAuth } from "../context/AuthProvider";
+import { z, ZodError } from "zod"
 import pkg from 'node-forge';
+
+const loginUserSchema = z.object({
+    email: z.string().email(),
+    passwordToHash: z.string()
+});
+
+type ILoginUserCredentials = z.infer<typeof loginUserSchema>;
 
 function Login() {
     const emailRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
     const { logUserIn } = useAuth() as AuthData;
-
-
+    
     console.log("Environment mode:" + import.meta.env.MODE);
 
     const [error, setError] = useState<string>("");
@@ -21,24 +29,21 @@ function Login() {
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        const email = emailRef.current!.value
-        const passwordToHash = passwordRef.current!.value;
-
-        if (email === "" || passwordToHash === "") {
-            setError("Please fill in all fields.");
-            return;
-        }
-
-        const salt = `${import.meta.env.VITE_TEST_SALT}`;;
-        const hashObject = pkg.md.sha512.create();
-        hashObject.update(salt + passwordToHash);
-        const hashedPassword = pkg.util.encode64(hashObject.digest().data);
-        
-        setError("");
-
         try {
+            const userCredentials = await loginUserSchema.parseAsync({
+                email: emailRef.current!.value,
+                passwordToHash: passwordRef.current!.value
+            } as ILoginUserCredentials);
+
+            const salt = `${import.meta.env.VITE_TEST_SALT}`;;
+            const hashObject = pkg.md.sha512.create();
+            hashObject.update(salt + userCredentials.passwordToHash);
+            const hashedPassword = pkg.util.encode64(hashObject.digest().data);
+
+            setError("");
+
             const formData = axios.toFormData({
-                email: email,
+                email: userCredentials.email,
                 password: hashedPassword
             })
 
@@ -53,13 +58,22 @@ function Login() {
         }
         catch (err: unknown) {
             if (isAxiosError(err)) {
-                if (err?.response) {
+                if (err.response) {
                     const email = emailRef.current!.value;
 
-                    setError(`Error Logging In: Email: ${email} Status: ${err.response.status} ${err.response.statusText}`);
+                    setError(`Error while LoggingIn: Email: ${email} Status: ${err.response.status} ${err.response.data}`);
                 }
                 else {
-                    setError(`Error Logging in: ${err}`);
+                    setError(`Error LoggingIn: ${err}`);
+                    console.error(err);
+                }
+            }
+            else if (err instanceof ZodError) {
+                if (err.issues.length) {
+                    const currentIssue = err.issues[0]
+
+                    const errorIssue = formatZodIssue(currentIssue)
+                    setError(`Validation Error while LoggingIn: ${errorIssue}`);
                     console.error(err);
                 }
             }
